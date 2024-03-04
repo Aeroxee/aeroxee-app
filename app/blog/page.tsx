e8515d65c1f077c2a73b153de97feec948324785
Aeroxee <vipercode305@gmail.com>
2024-03-04 22:30:43 +0700

    Highligh code to blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,4 @@
+import Code from "@/components/code";
 import Container from "@/components/container";
 import HeaderBlog from "@/components/header-blog";
 import { ClientDB } from "@/libs/db";
@@ -26,38 +27,41 @@ export default async function Blog() {
   const isLogined = cookieStore.get("isLogined");
 
   return (
-    <Container>
-      <h1 className="text-2xl text-white font-extrabold mb-4">Blog Saya</h1>
+    <>
+      <Code />
+      <Container>
+        <h1 className="text-2xl text-white font-extrabold mb-4">Blog Saya</h1>
 
-      {isLogined && <HeaderBlog />}
+        {isLogined && <HeaderBlog />}
 
-      <div className="md:w-[60%] md:mx-auto">
-        {blogs.map((blog: any, index: Key) => (
-          <div key={index} className="p-4 border-b border-gray-300">
-            <Link
-              href={`/blog/${blog._id}`}
-              className="text-xl text-sky-600 underline font-extrabold"
-              prefetch={false}
-            >
-              {blog.title}
-            </Link>
-            <div className="flex items-center flex-wrap mb-4">
-              <div className="flex items-center gap-1 text-xs text-gray-300 me-4">
-                <FontAwesomeIcon icon={faUser} />
-                <span>{blog.fullName}</span>
+        <div className="md:w-[60%] md:mx-auto">
+          {blogs.map((blog: any, index: Key) => (
+            <div key={index} className="p-4 border-b border-gray-300">
+              <Link
+                href={`/blog/${blog._id}`}
+                className="text-xl text-sky-600 underline font-extrabold"
+                prefetch={false}
+              >
+                {blog.title}
+              </Link>
+              <div className="flex items-center flex-wrap mb-4">
+                <div className="flex items-center gap-1 text-xs text-gray-300 me-4">
+                  <FontAwesomeIcon icon={faUser} />
+                  <span>{blog.fullName}</span>
+                </div>
+                <div className="flex items-center gap-1 text-xs text-gray-300 me-4">
+                  <FontAwesomeIcon icon={faClock} />
+                  <span>{getMoment(blog.createdAt)}</span>
+                </div>
               </div>
-              <div className="flex items-center gap-1 text-xs text-gray-300 me-4">
-                <FontAwesomeIcon icon={faClock} />
-                <span>{getMoment(blog.createdAt)}</span>
-              </div>
+
+              <p className="text-sm text-white font-light">
+                {stripHtmlAndTruncate(blog.content, 20)}
+              </p>
             </div>
-
-            <p className="text-sm text-white font-light">
-              {stripHtmlAndTruncate(blog.content, 20)}
-            </p>
-          </div>
-        ))}
-      </div>
-    </Container>
+          ))}
+        </div>
+      </Container>
+    </>
   );
 }
